8a636e923946243807092d6ff5742a568c1e04ba
iuliangeorgepetre <petregeorge19@stud.ase.ro>
2024-11-11 18:26:55 +0200

    change astro deploy settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-// Import the Vercel adapter
-import vercel from '@astrojs/vercel/serverless';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
+  output: "server",
+  adapter: cloudflare(),
 });
